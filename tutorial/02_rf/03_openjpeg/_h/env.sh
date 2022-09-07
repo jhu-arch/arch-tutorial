a17502971d0d37d25daf51deb16dd799b6fb65ec
Ricardo S. Jacomini <ricardo.jacomini@jhu.edu>
2022-09-06 20:24:05 -0400

    updates

--- tutorial/02_rf/03_openjpeg/_h/env.sh
+++ tutorial/02_rf/03_openjpeg/_h/env.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 module load gcc
 
 VERSION=2.4.0
@@ -9,4 +7,3 @@ PREFIX=${HOME}'/softwares_compiled/'${APP_NAME}'/'$VERSION
 export OPENJPEG2_CFLAGS=-I$PREFIX/usr/local/binclude
 export OPENJPEG2_LIBS=-L$PREFIX/usr/local/lib
 export PATH=$PREFIX/usr/local/bin:$PATH
-
