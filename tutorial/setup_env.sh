e60e54d2015abb380a4702b5fa6947f9a9723b04
Ricardo S. Jacomini <ricardo.jacomini@jhu.edu>
2022-09-07 09:43:28 -0400

    updates

--- tutorial/setup_env.sh
+++ tutorial/setup_env.sh
@@ -54,5 +54,5 @@ git pull origin main
 https://marcc.rtfd.io/RF.html
 https://marcc.rtfd.io/Snakemake.html
 
-https://snakemake.readthedocs.io/en/v3.10.1/getting_started/examples.html
 https://snakemake.readthedocs.io/en/stable/tutorial/basics.html
+https://snakemake.readthedocs.io/en/v3.10.1/getting_started/examples.html
